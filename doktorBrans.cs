0db3593b574dfea7316e33832611893eba761f61
yusufciante <104021412+yusufciante@users.noreply.github.com>
2023-12-28 23:46:12 +0300

    Add files via upload

---
+++ doktorBrans.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace hastanerandevu.Models
+{
+    public class DoktorBrans
+    {
+        [Key] // pk
+        public int Id { get; set; }
+        [Required] //not null
+        public string Ad { get; set; }
+    }
+}
